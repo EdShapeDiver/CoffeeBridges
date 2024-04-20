24c6ceeb94258c5bf3cf929951d74940f5589c2a
EdShapeDiver <110389955+EdShapeDiver@users.noreply.github.com>
2024-04-20 13:15:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# VIKTOR template for codespaces
+This template serves as a quickstart to start developing VIKTOR applications.
+
+Please consult the [cloud-based development guide](https://docs.viktor.ai/docs/getting-started/installation/remote-development/) for a detailed explanation on how to setup your codespace.
---
+++ app.py
@@ -0,0 +1,13 @@
+# Welcome to VIKTOR!
+#
+# First time using VIKTOR?
+#   Keep this window open while we are activating your account.
+#
+#   You will see the following message in the window below when your Codespace has finished loading:
+#  
+#       The system check has been completed with the following conclusion:
+#
+#       V Your system is ready to use VIKTOR with isolation mode 'venv'
+#
+#   After that return to your VIKTOR environment where you will learn how to create your first app!
+#   Keep this Codespace open as this is the place where you will write your code.
